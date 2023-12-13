91fa6300872fc3df9ff8b97f8ac3ba5bd4cce244
Andy Morales <39770126+andy2002a@users.noreply.github.com>
2023-12-13 12:29:11 -0500

    Update appliance.py

--- appliance.py
+++ appliance.py
@@ -1,133 +1,151 @@
 import requests
 import json
 
-def getApplianceProperties(applianceUrl, accessToken, applianceId):
-    api_url = applianceUrl + applianceId
-    propertiesResponse = requests.get(api_url, headers={'Authorization': 'Bearer ' + accessToken})
-    applianceProperties = propertiesResponse.json()[0]['properties']
-    return applianceProperties
-
-def getApplianceMetadata(applianceUrl, accessToken, applianceId):
-    api_url = applianceUrl + 'metadata/' + applianceId + '/en'
-    metadataResponse = requests.get(api_url, headers={'Authorization': 'Bearer ' + accessToken})
-    applianceMetaData = metadataResponse.json()[0]['propertyMetadata']
-    return applianceMetaData
 
 def addPowerOnToJson(inputJson):
     # adds "Power On" to existing command
     z = json.loads(inputJson)
-    z[0]['properties'].update({"Power": "1"})
+    z[0]["properties"].update({"Power": "1"})
     return json.dumps(z)
 
-def sendAppliancePostRequest(applianceUrl, accessToken, jsonData):
-     response = requests.post(
-        applianceUrl,
-        headers     = {'Authorization': 'Bearer ' + accessToken, 'Content-Type': 'application/json'},
-        data        = jsonData
-     )
-     print(response)
 
 class appliance:
     def __init__(self, applianceUrl, accessToken, jsonData):
-        self.applianceUrl   = applianceUrl
-        self.accessToken    = accessToken
-        self.id             = jsonData['id']
-        self.name           = jsonData['name']
-        self.type           = jsonData['type']
-        self.status         = jsonData['status']
+        self._applianceUrl = applianceUrl
+        self._accessToken = accessToken
+        self._id = jsonData["id"]
+        self._name = jsonData["name"]
+        self._type = jsonData["type"]
+        self._status = jsonData["status"]
         self.updateProperties()
         self.getCurrentStatus()
 
+    def getApplianceProperties(self):
+        api_url = self.applianceUrl + self.id
+        propertiesResponse = requests.get(
+            api_url, headers={"Authorization": "Bearer " + self.accessToken}
+        )
+        applianceProperties = propertiesResponse.json()[0]["properties"]
+        return applianceProperties
+
     def updateProperties(self):
-        newProperties = getApplianceProperties(self.applianceUrl, self.accessToken, self.id)
-        self.power               = newProperties['Power']
-        self.temperatureUnit     = newProperties['TemperatureUnit']
-        self.setTemperature      = newProperties['SetTemperature']
-        self.currentTemperature  = newProperties['CurrentTemperature']
-        self.mode                = newProperties['Mode']
-        self.fanSpeed            = newProperties['FanSpeed']
+        newProperties = self.getApplianceProperties()
+        self._power = newProperties["Power"]
+        self._temperatureUnit = newProperties["TemperatureUnit"]
+        self._setTemperature = newProperties["SetTemperature"]
+        self._currentTemperature = newProperties["CurrentTemperature"]
+        self._mode = newProperties["Mode"]
+        self._fanSpeed = newProperties["FanSpeed"]
 
     def getCurrentStatus(self):
-        outputStr = 'Current Device Status:\n'
-        outputStr += '\nname:                ' + self.name
-        outputStr += '\nid:                  ' + self.id
-        outputStr += '\nstatus:              ' + self.status
-        outputStr += '\ntype:                ' + self.type
-        outputStr += '\nPower:               ' + self.power
-        outputStr += '\ntemperatureUnit:     ' + self.temperatureUnit
-        outputStr += '\nsetTemperature:      ' + self.setTemperature
-        outputStr += '\ncurrentTemperature:  ' + self.currentTemperature
-        outputStr += '\nmode:                ' + self.mode
-        outputStr += '\nfanSpeed:            ' + self.fanSpeed
-        outputStr += '\n'
+        outputStr = "Current Device Status:\n"
+        outputStr += "\nname:                " + self._name
+        outputStr += "\nid:                  " + self._id
+        outputStr += "\nstatus:              " + self._status
+        outputStr += "\ntype:                " + self._type
+        outputStr += "\nPower:               " + self._power
+        outputStr += "\ntemperatureUnit:     " + self._temperatureUnit
+        outputStr += "\nsetTemperature:      " + self._setTemperature
+        outputStr += "\ncurrentTemperature:  " + self._currentTemperature
+        outputStr += "\nmode:                " + self._mode
+        outputStr += "\nfanSpeed:            " + self._fanSpeed
+        outputStr += "\n"
         return outputStr
 
+    def getApplianceMetadata(self):
+        api_url = self._applianceUrl + "metadata/" + self._id + "/en"
+        metadataResponse = requests.get(
+            api_url, headers={"Authorization": "Bearer " + self._accessToken}
+        )
+        applianceMetaData = metadataResponse.json()[0]["propertyMetadata"]
+        return applianceMetaData
+
     def updateMetadata(self):
-        newMetadata = getApplianceMetadata(self.applianceUrl, self.accessToken, self.id)
+        newMetadata = self.getApplianceMetadata()
         for metaDataObject in newMetadata:
-            if (metaDataObject['key'] == 'FanSpeed'):
-                fanValues = metaDataObject['enumValues']
-                self.fanValues = {}
+            if metaDataObject["key"] == "FanSpeed":
+                fanValues = metaDataObject["enumValues"]
+                self._fanValues = {}
                 for value in fanValues:
-                        self.fanValues.update({fanValues[value]['key']: value})
-            elif (metaDataObject['key'] == 'SetTemperature'):
-                self.minValueCelsius = metaDataObject['minValueCelsius']
-                self.maxValueCelsius = metaDataObject['maxValueCelsius']
-                self.minValueFahrenheit = metaDataObject['minValueFahrenheit']
-                self.maxValueFahrenheit = metaDataObject['maxValueFahrenheit']
-            elif (metaDataObject['key'] == 'TemperatureUnit'):
-                tempUnitValues = metaDataObject['enumValues']
-                self.possibleTemperatureUnits = {}
+                    self._fanValues.update({fanValues[value]["key"]: value})
+            elif metaDataObject["key"] == "SetTemperature":
+                self._minValueCelsius = metaDataObject["minValueCelsius"]
+                self._maxValueCelsius = metaDataObject["maxValueCelsius"]
+                self._minValueFahrenheit = metaDataObject["minValueFahrenheit"]
+                self._maxValueFahrenheit = metaDataObject["maxValueFahrenheit"]
+            elif metaDataObject["key"] == "TemperatureUnit":
+                tempUnitValues = metaDataObject["enumValues"]
+                self._possibleTemperatureUnits = {}
                 for value in tempUnitValues:
-                        self.possibleTemperatureUnits.update({tempUnitValues[value]['label']: value})
-            elif (metaDataObject['key'] == 'Mode'):
-                self.systemModes = {'Fan only': '0', 'Heat': '1', 'Cool': '2', 'Dry': '3'}  ## Remove this when bug resolved
+                    self._possibleTemperatureUnits.update(
+                        {tempUnitValues[value]["label"]: value}
+                    )
+            elif metaDataObject["key"] == "Mode":
+                self._systemModes = {
+                    "Fan only": "0",
+                    "Heat": "1",
+                    "Cool": "2",
+                    "Dry": "3",
+                }  ## Remove this when bug resolved
                 # There is a bug where the API does not match the modes correctly
                 #
-                #systemModes = metaDataObject['enumValues']
-                #self.systemModes = {}
-                #for mode in systemModes:
+                # systemModes = metaDataObject['enumValues']
+                # self.systemModes = {}
+                # for mode in systemModes:
                 #        self.systemModes.update({systemModes[mode]['key']: mode})
 
-    def changeTemperature(self, newTempValue, tempUnit, powerOnWithChange):
-        jsonDataForPost = json.dumps(
-            [{
-                'id': self.id,
-                'properties': {
-                        'SetTemperature': str(newTempValue),
-                        'TemperatureUnit': self.possibleTemperatureUnits[tempUnit]
+    def sendAppliancePostRequest(self, jsonData):
+        response = requests.post(
+            self._applianceUrl,
+            headers={
+                "Authorization": "Bearer " + self._accessToken,
+                "Content-Type": "application/json",
+            },
+            data=jsonData,
+        )
+        print(response)
+
+    def jsonPropertiesBuilder(self, propertiesDict):
+        jsonData = json.dumps(
+            [
+                {
+                    "id": self._id,
+                    "properties": propertiesDict,
                 }
-            }]
+            ]
+        )
+        return jsonData
+
+    def setTemperature(self, newTempValue, powerOnWithChange):
+        jsonDataForPost = self.jsonPropertiesBuilder(
+            {"SetTemperature": str(newTempValue)}
         )
         # Also power on the device if the temperature is changed
         if powerOnWithChange:
             jsonDataForPost = addPowerOnToJson(jsonDataForPost)
-        sendAppliancePostRequest(self.applianceUrl, self.accessToken, jsonDataForPost)
+        #
+        self.sendAppliancePostRequest(jsonDataForPost)
 
-    def setFanSpeed(self, fanSpeed, powerOnWithChange):
-        jsonDataForPost = json.dumps(
-            [{
-                'id': self.id,
-                'properties': {
-                        'FanSpeed': str(self.fanValues[fanSpeed])
-                }
-            }]
+    def setTemperatureUnit(self, tempUnit):
+        jsonDataForPost = self.jsonPropertiesBuilder(
+            {"TemperatureUnit": self._possibleTemperatureUnits[tempUnit]}
+        )
+        self.sendAppliancePostRequest(jsonDataForPost)
+
+    def setFanSpeed(self, fanSpeed, powerOnWithChange=False):
+        jsonDataForPost = self.jsonPropertiesBuilder(
+            {"FanSpeed": str(self._fanValues[fanSpeed])}
         )
         # Also power on the device if the speed is changed
         if powerOnWithChange:
             jsonDataForPost = addPowerOnToJson(jsonDataForPost)
-        sendAppliancePostRequest(self.applianceUrl, self.accessToken, jsonDataForPost)
+        self.sendAppliancePostRequest(jsonDataForPost)
 
-    def setSystemMode(self, systemMode, powerOnWithChange):
-        jsonDataForPost = json.dumps(
-            [{
-                'id': self.id,
-                'properties': {
-                        'Mode': str(self.systemModes[systemMode])
-                }
-            }]
+    def setSystemMode(self, systemMode, powerOnWithChange=False):
+        jsonDataForPost = self.jsonPropertiesBuilder(
+            {"Mode": str(self._systemModes[systemMode])}
         )
         # Also power on the device if the speed is changed
         if powerOnWithChange:
             jsonDataForPost = addPowerOnToJson(jsonDataForPost)
-        sendAppliancePostRequest(self.applianceUrl, self.accessToken, jsonDataForPost)
+        self.sendAppliancePostRequest(jsonDataForPost)
